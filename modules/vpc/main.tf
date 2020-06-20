e3872f33c8d0fb318ef32cd6d8391a85a7ea805c
Gareth Rushgrove <gareth@morethanseven.net>
2020-06-20 12:12:47 +0100

    Terraform fmt error

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -2,6 +2,17 @@ resource "aws_vpc" "example" {
   cidr_block = var.cidr
 }
 
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.example.id
+
+  ingress {
+    protocol  = -1
+    self      = true
+    from_port = 0
+    to_port   = 0
+  }
+}
+
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allow SSH inbound from anywhere"

9a70c4da38603e3207dc492af78dd8dbb22fea4f
Gareth Rushgrove <gareth@morethanseven.net>
2020-06-25 07:45:52 +0100

    Ignore missing KMS key for now

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -3,7 +3,7 @@ resource "aws_s3_bucket" "secure-bucket" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.mykey.arn
+#        kms_master_key_id = aws_kms_key.mykey.arn
         sse_algorithm     = "AES256"
       }
     }
